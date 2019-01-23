7bc1dfe1c618ff5ae39f9d33696ae3f3e7245662
asiagod <476494273@qq.com>
2019-01-23 08:57:47 +0800

    IU-2018.1.4 <Admin@yzcui Create baseRefactoring.xml, markdown.xml, Vibrant Unknown (Darcula).icls, ide.general.xml, git.xml, _@user_Default.icls, pom.xml, print.xml, CodeGlance.xml, project.default.xml, rainbow_brackets.xml, Default copy.xml, lombok-plugin.xml, other.xml, yiiguxing.translation.xml, Default.xml, ignore.xml, Nice Python.icls, find.xml, ui.lnf.xml, filetypes.xml, keymap.xml, plugin_ui.xml, colors.scheme.xml, notifications.xml, databaseDrivers.xml, mavenVersion.xml, templates.xml, javarebel.xml, stringManipulation.xml, consoleFolding.xml, Default.xml, laf.xml, Class.java, jdk.table.xml, debugger.renderers.xml, path.macros.xml, codeHelper.xml, mavenRunHelper.xml, MyTemplate.xml, vcs.xml, editor.codeinsight.xml, databaseSettings.xml, toStringTemplates.xml, mybatis.xml, Mybatis_SQL.xml, editor.xml, debugger.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,12 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+*  
+* <p>DESCRIPTION:</p>
+*
+* @author AsiaCui
+* @version: 1.0.0
+* @create ${YEAR}-${MONTH}-${DAY} ${TIME}
+**/
+public class ${NAME} {
+}
